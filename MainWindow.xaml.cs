e90357142ee634dee7afc19dc4c2813e0c619b32
ОКСАНА Аркадьева <oksana.arkadeva29@gmail.com>
2024-04-11 23:07:22 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Library.Components;
+using Library.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Library
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Navigation.mainWindow = this;
+           Navigation.NextPage(new PageComponent("Авторизация",new AuthorizationPage()));
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.BackPage();
+        }
+
+        private void ForwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoForward)
+                MainFrame.GoForward();
+
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Library.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Library.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EntryBtn_Click(object sender, RoutedEventArgs e)
+        {
+            App.isAdmin = true;
+            Navigation.NextPage(new PageComponent("Библиотека", new AdminMenuPage()));
+         
+        }
+    }
+}
